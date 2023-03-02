5f29b858bfe11b40e5349557ec141e12509726ab
Artem09222 <107929764+Artem09222@users.noreply.github.com>
2023-03-02 11:06:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//Exersice 1
+
+let numbers = [4, 1, 3, 2, 7, 5, 6, 9, 8, 10];
+console.log(numbers);
+console.log(numbers.length);
+
+//Exersice 2
+
+let names = ["Artem", "Vitalik", "Tema", "Vetal", "Sonya", "Sasha", "Vasil", "Nazar", "Vlad", "Danya"];
+console.log(names);
+console.log(names[2]);
+console.log(names[5]);
+console.log(names[9]);
+
+//Exersice 3
+
+let numes = [];
+console.log(numes);
+numes[0] = 10;
+console.log(numes);
+numes[1] = 8;
+console.log(numes);
+numes[5] = 100;
+console.log(numes);
